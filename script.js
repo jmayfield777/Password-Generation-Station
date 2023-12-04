629fcbe8d7f16ec68a3172dd4f5b794ee1a35db1
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2023-12-04 15:33:50 -0500

    added password length variable and if then statement to validate password length

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ var specials = "!@#$%^&*";
 
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("Enter a number between 8 and 128: ");
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a number between 8 and 128.");
+    return;
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
